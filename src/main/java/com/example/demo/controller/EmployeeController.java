2110276c7e88e364e78df68b8a77be53173c66d5
kuharon1994 <5635512156@emai.psu.ac.th>
2023-08-09 11:06:12 +0700

    lab4

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,66 +21,75 @@ import com.example.demo.repository.EmployeeRepository;
 
 @RestController
 public class EmployeeController {
-	
+
 	@Autowired
 	EmployeeRepository employeeRepository;
 
-
 	@GetMapping("/employee")
-	public List<Employee> getEmployee() {
-		return employeeRepository.findAll();
+	public ResponseEntity<Object> getEmployee() {
+		try {
+			
+			Integer a = null;
+			a++;
+			
+			List<Employee> employees = employeeRepository.findAll(); 
+			
+			return new ResponseEntity<>(employees, HttpStatus.OK);
+
+		} catch (Exception e) {
+			
+			return new ResponseEntity<>("Internal server error", HttpStatus.BAD_REQUEST);
+		}
+
 	}
 
 	@PostMapping("/employee")
 	public Employee addEmployee(@RequestBody Employee body) {
-				
+
 		return employeeRepository.save(body);
 	}
 
 	@GetMapping("/employee/{employeeId}")
 	public Optional<Employee> getEmployeeDetail(@PathVariable Integer employeeId) {
-		
+
 		Optional<Employee> employee = employeeRepository.findById(employeeId);
-		
+
 		return employee;
 	}
-	
+
 	@PutMapping("employee/{employeeId}")
 	public Employee updateEmployee(@PathVariable Integer employeeId, @RequestBody Employee body) {
-		
+
 		Optional<Employee> employee = employeeRepository.findById(employeeId);
-		
-		if(employee.isPresent()) {
-			Employee employeeEdit = employee.get(); 
+
+		if (employee.isPresent()) {
+			Employee employeeEdit = employee.get();
 			employeeEdit.setFirstName(body.getFirstName());
 			employeeEdit.setLastName(body.getLastName());
 			employeeEdit.setSalary(body.getSalary());
 			employeeEdit.setEmployeeId(body.getEmployeeId());
-			
+
 			employeeRepository.save(employeeEdit);
-			
+
 			return employeeEdit;
-		}else {
+		} else {
 			return null;
 		}
-		
+
 	}
-	
-	
+
 	@DeleteMapping("employee/{employeeId}")
 	public String deleteEmployee(@PathVariable Integer employeeId) {
 
 		Optional<Employee> employee = employeeRepository.findById(employeeId);
-		
-		if(employee.isPresent()) {
+
+		if (employee.isPresent()) {
 			employeeRepository.delete(employee.get());
-			
+
 			return "DELETE SUCSESS";
-		}else {
+		} else {
 			return "Employee not found";
 		}
 	}
-	
-	
 
 }
